4f84e64fc47adb04304eb32b1f0ac9711f0c2ce5
Ron-Rocks <57868441+Ron-Rocks@users.noreply.github.com>
2019-12-03 16:20:31 +0530

    Update MnistFunctionalAPI.py

--- MnistFunctionalAPI.py
+++ MnistFunctionalAPI.py
@@ -7,14 +7,16 @@ import numpy as np
 mnist = tf.keras.datasets.mnist
 (xTrain,yTrain),(xTest,yTest) = mnist.load_data()
 
-xTrain = (xTrain.reshape(-1,784))/255
-xTest = (xTest.reshape(-1,784))/255
+xTrain = xTrain.reshape(-1,28,28,1)/255
+xTest = xTest.reshape(-1,28,28,1)/255
 
-inputLayer = keras.Input(shape = (784,))
+inputLayer = keras.Input(shape = (28,28,1))
 
-x = layers.Dense(64,activation = "relu")(inputLayer)
+x = layers.Conv2D(32,3,activation = "relu")(inputLayer)
 
-x =layers.Dense(128,activation = "relu")(x)
+x =layers.Conv2D(16,3,activation = "relu")(x)
+
+x = layers.Flatten()(x)
 
 x = layers.Dense(64,activation = "relu")(x)
 
@@ -25,10 +27,11 @@ model = keras.Model(inputs = inputLayer,outputs = outputLayer)
 
 model.compile(loss = "sparse_categorical_crossentropy",optimizer = "adam",metrics = ["accuracy"])
 
-model.fit(xTrain,yTrain,validation_split = 0.3,epochs = 5)
+model.fit(xTrain,yTrain,validation_split = 0.3,epochs = 3)
 
 prediction = model.predict(xTest)
 
 print(np.argmax(prediction[9]))
 plt.imshow(xTest[9].reshape(28,28))
 plt.show()
+
