a93b293cabb36d7232d4b223ae0682d558e1f9ad
Ron-Rocks <57868441+Ron-Rocks@users.noreply.github.com>
2020-01-17 10:40:00 +0530

    Update MnistFunctionalAPI.py

--- MnistFunctionalAPI.py
+++ MnistFunctionalAPI.py
@@ -13,24 +13,26 @@ xTest = xTest.reshape(-1,28,28,1)/255
 inputLayer = keras.Input(shape = (28,28,1))
 
 x = layers.Conv2D(32,3,activation = "relu")(inputLayer)
-
 x =layers.Conv2D(16,3,activation = "relu")(x)
-
 x = layers.Flatten()(x)
 
 x = layers.Dense(64,activation = "relu")(x)
 
 outputLayer = layers.Dense(10,activation = "softmax")(x)
 
+
 model = keras.Model(inputs = inputLayer,outputs = outputLayer)
-
-
 model.compile(loss = "sparse_categorical_crossentropy",optimizer = "adam",metrics = ["accuracy"])
 
-model.fit(xTrain,yTrain,validation_split = 0.3,epochs = 3)
+model.fit(xTrain,yTrain,validation_data=(xTest,yTest),epochs = 3)
+evaluationResults= model.evaluate(xTest,yTest)
+
+print("Evaluation Results :  Loss - ",evaluationResults[0],"  Accuracy - ",evaluationResults[1])
 
 prediction = model.predict(xTest)
 
-print(np.argmax(prediction[9]))
-plt.imshow(xTest[9].reshape(28,28))
+randomIndex = np.random.randint(0,50)
+
+print('Predicted Number - ',np.argmax(prediction[randomIndex]))
+plt.imshow(xTest[randomIndex].reshape(28,28))
 plt.show()
